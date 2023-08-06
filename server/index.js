dadcc10fab8b5d1361ebdb0c5e6a457625c34fb0
jacklynchiaentsai <jacklyntsai1213@gmail.com>
2023-08-06 18:50:56 +0800

    initiated backend arch

--- server/index.js
+++ server/index.js
@@ -9,3 +9,12 @@ const app = express();
 app.use(cors());
 app.use(express.json({limit: '50mb'}))
 
+app.get('/', async (req, res) => {
+    res.send('Hello World');
+})
+
+const startServer = async () => {
+    app.listen(8080, () => console.log('Server has started on port http://localhost:8080'))
+}
+
+startServer()
